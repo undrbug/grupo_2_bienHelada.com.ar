d9933dd14dababfad4b3142b61f533afb33e0a6e
AumadaFranco <aumadafranco@gmail.com>
2024-06-21 11:31:09 -0300

    Agregando html login

---
+++ app.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+
+// app.use(express.static("./public"));
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.resolve("views/home.html"))
+// })
+
+// app.listen(3700, () =>
+//     console.log("Server running on http://localhost:3700")
+//  )
