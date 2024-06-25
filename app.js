c6b0df6cf1e852cf08abbc3dcf7a088b25edf876
Israel Campillay <israelcampy@gmail.com>
2024-06-25 13:22:34 -0300

    se agrega productCard(carrito de compras) y el archivo RETRO.md

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-// const express = require("express");
-// const app = express();
-// const path = require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-// app.use(express.static("./public"));
+ app.use(express.static("./public"));
 
-// app.get("/", function(req, res) {
-//     res.sendFile(path.resolve("views/home.html"))
-// })
+ app.get("/", function(req, res) {
+    res.sendFile(path.resolve("views/home.html"))
+})
 
-// app.listen(3700, () =>
-//     console.log("Server running on http://localhost:3700")
-//  )
+app.get("/home.html", function(req, res) {
+    res.sendFile(path.resolve("views/home.html"))
+})
+
+app.get("/login.html", function(req, res) {
+    res.sendFile(path.resolve("views/login.html"))
+})
+app.get("/registro.html", function(req, res) {
+    res.sendFile(path.resolve("views/registro.html"))
+})
+app.get("/productCart.html", function(req, res) {
+    res.sendFile(path.resolve("views/productCart.html"))
+})
+ app.listen(3700, () =>
+   console.log("Server running on http://localhost:3700")
+ )
