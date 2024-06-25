45f835ce0e309288b004a1731fbbb5c599158b97
Israel Campillay <israelcampy@gmail.com>
2024-06-25 15:53:37 -0300

    configurando las rutas y manejadores de archivos estáticos en Express

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ app.get("/registro.html", function(req, res) {
 app.get("/productCart.html", function(req, res) {
     res.sendFile(path.resolve("views/productCart.html"))
 })
+app.get("/detalleProducto.html", function(req, res) {
+    res.sendFile(path.resolve("views/detalleProducto.html"))
+})
+app.get("/recuperarPassword.html", function(req, res) {
+    res.sendFile(path.resolve("views/recuperarPassword.html"))
+})
+app.get("/restablecerPassword.html", function(req, res) {
+    res.sendFile(path.resolve("views//restablecerPassword.html"))
+})
  app.listen(3700, () =>
    console.log("Server running on http://localhost:3700")
  )
