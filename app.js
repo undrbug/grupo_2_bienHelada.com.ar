b7517f7ebd85832ed8ec6e706023fda7c841f24a
Juan torres <juanmanu3365@gmail.com>
2024-07-02 16:58:43 -0300

    index, stylos y server andando

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/productDetail.html', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/productCart.html', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
