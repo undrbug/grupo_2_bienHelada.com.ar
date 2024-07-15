8804e8ec6ceec4c7d55144ea97871ddbd73235fa
hGauna <undrbug@gmail.com>
2024-07-15 18:46:12 -0300

    cambio de extension html a ejs

--- app.js
+++ app.js
@@ -4,24 +4,22 @@ const path = require("path");
 
  app.use(express.static("./public"));
 
+ const PORT = process.env.PORT || 3700;
+
  app.get("/", function(req, res) {
     res.sendFile(path.resolve("views/home.html"))
 })
 
-app.get("/home.html", function(req, res) {
-    res.sendFile(path.resolve("views/home.html"))
-})
-
-app.get("/login.html", function(req, res) {
+app.get("/login", function(req, res) {
     res.sendFile(path.resolve("views/login.html"))
 })
-app.get("/registro.html", function(req, res) {
+app.get("/registro", function(req, res) {
     res.sendFile(path.resolve("views/registro.html"))
 })
-app.get("/productCart.html", function(req, res) {
+app.get("/productcart", function(req, res) {
     res.sendFile(path.resolve("views/productCart.html"))
 })
-app.get("/detalleProducto.html", function(req, res) {
+app.get("/detalleproducto", function(req, res) {
     res.sendFile(path.resolve("views/detalleProducto.html"))
 })
 app.get("/recuperarPassword.html", function(req, res) {
@@ -30,6 +28,6 @@ app.get("/recuperarPassword.html", function(req, res) {
 app.get("/restablecerPassword.html", function(req, res) {
     res.sendFile(path.resolve("views//restablecerPassword.html"))
 })
- app.listen(3700, () =>
+ app.listen(PORT, () =>
    console.log("Server running on http://localhost:3700")
  )
