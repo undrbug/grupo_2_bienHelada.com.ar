92cad86a091bf79a4917108d02405bb451e98258
Saúl Hernán Gauna <hgauna@iprodha.misiones.gov.ar>
2024-07-18 08:33:56 -0300

    add main controller al home

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,13 +2,5 @@ const router = require('express').Router();
 const mainController = require('../controllers/main.controller.js');
 
 router.get('/', mainController.home);
-router.get("/login", mainController.login);
-router.get('/product', mainController.product);
-router.get("/register", mainController.register);
-router.get('/productcart', mainController.productCart);
-router.get('/productdetail', mainController.productDetail);
-router.get("/recuperarpassword", mainController.recuperarPassword);
-router.get("/restablecerpasword", mainController.restablecerPassword);
-
 
 module.exports = router;
