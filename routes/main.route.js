f73c5e679e3ac94a4a925524c791b843e2c1b688
juan33torres <juanmanu3365@gmail.com>
2024-07-17 22:10:29 -0300

    Json, y produtc.ejs

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const mainController = require('../controllers/main.controller.js');
 
 router.get('/', mainController.home);
-router.get('/login', mainController.login);
-router.get('/register', mainController.register);
+router.get("/login", mainController.login);
+router.get('/product', mainController.product);
+router.get("/register", mainController.register);
 router.get('/productcart', mainController.productCart);
-router.get('/productdetail/:id', mainController.productDetail);
-router.get('/recuperarpassword', mainController.recuperarPassword);
-router.get('/restablecerpasword', mainController.restablecerPassword);
+router.get('/productdetail', mainController.productDetail);
+router.get("/recuperarpassword", mainController.recuperarPassword);
+router.get("/restablecerpasword", mainController.restablecerPassword);
 
 
 module.exports = router;
