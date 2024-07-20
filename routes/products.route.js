c9702854a82c051e364893c733b13a7b5696e6b7
hGauna <undrbug@gmail.com>
2024-07-20 18:07:33 -0300

    se agrego rutas para el crud de productos

--- routes/products.route.js
+++ routes/products.route.js
@@ -4,6 +4,9 @@ const productsController = require('../controllers/products.controller.js');
 router.get('/products', productsController.products);
 router.get('/productcart', productsController.productCart);
 router.get('/productdetail', productsController.productDetail);
+router.get('/productadd', productsController.productAdd);
+router.get('/productdel', productsController.productDel);
+router.get('/productmod', productsController.productMod);
 
 module.exports = router;
 
