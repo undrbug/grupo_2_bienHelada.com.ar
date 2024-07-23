bf99554028c5456578ae36440db71c6112e05b9f
hGauna <undrbug@gmail.com>
2024-07-23 00:06:01 -0300

    se quito async await en save()

--- services/dataSource.js
+++ services/dataSource.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const path = require("path");
 
 const datasource = {
-  filePath: path.resolve(__dirname, "../data/wines.json"),
+  filePath: path.resolve(__dirname, '../data/wines.json'),
 
   load() {
-    const products =  fs.readFileSync(this.filePath, "");
+    const products =  fs.readFileSync(this.filePath, "utf-8");
     const productsJson = JSON.parse(products);
     return productsJson;
   },
-  async save(data) {
+  save(data) {
     const jsonData = JSON.stringify(data);
-    await fs.writeFile(this.filePath, jsonData, "utf-8");
+    fs.writeFileSync(this.filePath, jsonData, "utf-8");
   },
 };
 
