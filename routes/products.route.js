8817301ddd7a3d874ed075c749749520b7346495
hGauna <undrbug@gmail.com>
2024-07-22 23:38:01 -0300

    agregue rutas productaddview y productadd

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const productsController = require('../controllers/products.controller.js');
 
-router.get('/products', productsController.products);
-router.get('/productcart', productsController.productCart);
+router.get('/', productsController.products);
+router.get('/cart', productsController.productCart);
 router.get('/productdetail', productsController.productDetail);
-router.get('/productadd', productsController.productAdd);
-router.get('/productdel', productsController.productDel);
-router.get('/productmod', productsController.productMod);
+router.get('/add', productsController.productAddView);
+router.post('/add', productsController.productAdd);
+// router.get('/productdel', productsController.productDelView);
+router.delete('/productdel/:id', productsController.productDel);
+// router.get('/productmod', productsController.productModView);
+router.put('/productmod/:id', productsController.productMod);
 
 module.exports = router;
 
