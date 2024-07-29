6b325588f3e50bb8f911379879de29103aaf9d12
hGauna <undrbug@gmail.com>
2024-07-29 12:37:33 -0300

    comentario añadido

--- routes/products.route.js
+++ routes/products.route.js
@@ -3,6 +3,7 @@ const productsController = require('../controllers/products.controller.js');
 
 //muestra todos los prodctos de manera dinamica
 router.get('/', productsController.products);
+//carrito de compras
 router.get('/cart', productsController.productCart);
 /// modificar para que reciba un :id
 router.get('/productdetail', productsController.productDetail);
