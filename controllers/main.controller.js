01f27c8f7c0b0f8b2347bc522f2df93d74f03158
hGauna <undrbug@gmail.com>
2024-07-29 12:33:59 -0300

    lista del home dinamica

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
+const data = require('../data/wines.json');
+
+const winesFilePath = path.join(__dirname, '../data/wines.json');
+const winesData = fs.readFileSync(winesFilePath, 'utf-8');
+const wineList = JSON.parse(winesData);
 
 const mainController = {
     home: (req, res) => {
-        res.render('index.ejs',{title: 'Bien-Heladas wines&drinks'});
+        res.render('index.ejs',{title: "Bien-Heladas wines&drinks", wineList: wineList});
     }
 };
 
