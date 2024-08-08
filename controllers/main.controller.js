a4a8a844b3bfd7c32cf5c4900c2dd1de9d7c3e1f
hGauna <undrbug@gmail.com>
2024-08-08 00:23:40 -0300

    renderizado de productos usando metodo service.load()

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -3,13 +3,14 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const data = require('../data/wines.json');
+const services = require("../services/dataSource.js");
 
-const winesFilePath = path.join(__dirname, '../data/wines.json');
-const winesData = fs.readFileSync(winesFilePath, 'utf-8');
-const wineList = JSON.parse(winesData);
+
 
 const mainController = {
     home: (req, res) => {
+        const wineList = services.load();
+        // res.send(wineList);
         res.render('index.ejs',{title: "Bien-Heladas wines&drinks", wineList: wineList});
     }
 };
