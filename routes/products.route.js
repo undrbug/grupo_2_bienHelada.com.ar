dc063191ffcd2c6142e84f2ee33e112871868f45
Hgauna <hgauna@iprodha.misiones.gov.ar>
2024-08-08 07:17:57 -0300

    agregamos comentarios en cada metodo del product controller

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,23 +2,23 @@ const router = require('express').Router();
 const productsController = require('../controllers/products.controller.js');
 const upload = require('../services/multerStorage.js');
 
-
-
 //muestra todos los prodctos de manera dinamica
 router.get('/', productsController.getAllProducts);
 //carrito de compras
 router.get('/cart', productsController.productCart);
-/// modificar para que reciba un :id
+//detalle de producto
 router.get('/productdetail/:id', productsController.productDetail);
+//Vista para agregar producto o dar de alta
 router.get('/add', productsController.productAddView);
+//Metodo para agregar producto con su middleware para cargar imagen
 router.post('/add', upload.single('image'), productsController.productAdd);
+//Vista para eliminar producto (no se si lo vamos a implementar)
 // router.get('/productdel', productsController.productDelView);
+//Metodo para eliminar producto
 router.delete('/productdel/:id', productsController.productDel);
-
-//modificar producto
+//Vista para modificar producto
 router.get('/productmod/:id', productsController.productModView);
+//Metodo para modificar producto con su middleware para cargar imagen
 router.put('/productmod/:id', upload.single('image'), productsController.productMod);
 
 module.exports = router;
-
-//routes para products
