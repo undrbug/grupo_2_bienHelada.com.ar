7ad1ac28c223b55969ce2b714dcf507d9fdba6e7
hGauna <undrbug@gmail.com>
2024-08-08 00:29:05 -0300

    feat: metodos buscar por id y update

--- services/dataSource.js
+++ services/dataSource.js
@@ -6,13 +6,28 @@ const datasource = {
 
   load() {
     const products =  fs.readFileSync(this.filePath, "utf-8");
+    //transformamos el json a un array de objetos
     const productsJson = JSON.parse(products);
     return productsJson;
   },
   save(data) {
     const jsonData = JSON.stringify(data);
+    
     fs.writeFileSync(this.filePath, jsonData, "utf-8");
   },
+  //update jscon file
+  update(data) {
+    let products = this.load();
+    products = products.filter((product) => product.id !== data.id);
+    products.push(data);
+    this.save(products);
+    
+  },
+  findProductById(id) {
+    const products = this.load();
+    const product = products.find((product) => product.id === id);
+    return product;
+  }
 };
 
 module.exports = datasource;
