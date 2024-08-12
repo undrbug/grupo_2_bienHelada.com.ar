c14b0b5a243ba741c581ab0e0e7f9340f3f8dda7
Hgauna <hgauna@iprodha.misiones.gov.ar>
2024-08-12 12:43:55 -0300

    Feat: add loginCheck method with bcrypt

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,11 +2,30 @@ const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
 const services = require("../services/dataUsers.js");
+const bcrypt = require('bcryptjs');
+const dataUsers = require("../services/dataUsers.js");
 
 
 const usersController = {
     login: (req, res) => {
         res.render('users/login.ejs', {title: 'Login'});
+    },
+    loginCheck: (req, res) => {
+        const user = dataUsers.findByEmail(req.body.email);
+        if (user) {
+            const passwordMatch = bcrypt.compareSync(req.body.password, user.password);
+            if (passwordMatch) {
+                //Redireccionar a la home o a la página de perfil en caso de éxito y muestre
+                //los datos del usuario en algún lugar del sitio, como el header.
+                res.send("Logueado correctamente");
+                // res.redirect("/");
+            } else {
+                //Redireccione nuevamente al login en caso de error.
+                res.send("login incorrecto");
+                // res.redirect("/login");
+            }
+        }
+        res.send("Usuario no encontrado");
     },
     register: (req, res) => {
         res.render('users/register.ejs', {title: 'Register'});
@@ -17,13 +36,13 @@ const usersController = {
             const imgUser = req.file
                 ? `/images/users/${req.file.filename}`
                 : "/images/users/default.png";
-                console.log(req.body);
             // Accede a los campos del formulario desde req.body
             const { firstName, lastName, email, password } = req.body;
     
             // Carga los usuarios existentes
             let users = services.load();
-    
+            //Encriptamos la contraseña
+            let passwordEnc = bcrypt.hashSync(password, 10);
             // Crea un nuevo usuario con todos los campos
             const newUser = {
                 id: users.length + 1,
@@ -31,7 +50,7 @@ const usersController = {
                 lastName: lastName,
                 email: email,
                 image: imgUser,
-                password: password,
+                password: passwordEnc,
             };
     
             // Agrega el nuevo usuario a la lista de usuarios y guarda los cambios
