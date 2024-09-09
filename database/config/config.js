097afb287335abf3f067596a251566da3ff573fd
Cainenk011 <yamal.e.maldonado@gmail.com>
2024-09-09 00:37:15 -0300

    change in stylessheet index, media queries - changes in the resolution of different screens

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "bienhelada",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: "Dek@377399474352110000",
+    database: "bienhelada",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
