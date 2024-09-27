ffec6c3dfce3f6d91b44afeedb39df89f6449714
hGauna <undrbug@gmail.com>
2024-09-26 21:35:23 -0300

    add error message in catch

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
-const data = require('../data/wines.json');
-const services = require("../services/dataSource.js");
-let db = require('../database/models');
-
-
+//const data = require('../data/wines.json');
+//const services = require("../services/dataSource.js");
+let db = require("../database/models");
 
 const mainController = {
-    home: (req, res) => {
+	home: (req, res) => {
+        db.Product.findAll()
+        .then(function(wineList) {
+            res.render("index.ejs", {title: "Home", wineList : wineList});
+        })
+        .catch(function(error) {
+            // Manejo de errores
+            console.error('Error al obtener productos:', error.message);
+            res.status(500).send('Error al obtener la lista de productos.');
+        });
 
-    
-    db.Products.findAll()
-    .then(function(wineList) {        
-        res.render('index.ejs',{title: "Bien-Heladas wines&drinks", wineList: wineList});
-    })
-    .catch(function(error) {
-        // Manejo de errores
-        console.error('Error al obtener productos:', error);
-        res.status(500).send('Error al obtener la lista de productos.');
-    });
 
-}
+
+    }
 };
 
 module.exports = mainController;
