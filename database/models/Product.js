7492ebc8a99d659de780967158203209821378ad
hGauna <undrbug@gmail.com>
2024-09-26 22:41:51 -0300

    change data type from enum to string

--- database/models/Product.js
+++ database/models/Product.js
@@ -1,64 +1,68 @@
 module.exports = (sequelize, DataTypes) => {
-const Products = sequelize.define('Products', {
-  ID_Product: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  name: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  drink_description: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  drink_type: {
-    type: DataTypes.ENUM('Cerveza', 'Vino', 'Whisky', 'Espumante', 'Licor'),
-    allowNull: false
-  },
-  Presentation: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false
-  },
-  Stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  brand: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  Barcode: {
-    type: DataTypes.STRING(20),
-    allowNull: true,
-    unique: true
-  },
-  Maturity: {
-    type: DataTypes.DATE,
-    allowNull: true
-  },
-  highlighted: {
-    type: DataTypes.BOOLEAN,
-    allowNull: true
-  },
-  offer: {
-    type: DataTypes.BOOLEAN,
-    allowNull: true
-  },
-  Image: {
-    type: DataTypes.STRING(255),
-    allowNull: true
-  }
-}, {
-  tableName: 'Product',
-  timestamps: false  
-});
-  
-  return Products;
-};
+    const Product = sequelize.define('Product', {
+      ID_Product: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      drink_description: DataTypes.TEXT,
+      drink_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Presentation: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+      },
+      Stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Barcode: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      Maturity: DataTypes.DATE,
+      highlighted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      offer: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      Image: {
+        type: DataTypes.STRING,
+        defaultValue: '../images/products/default.jpg'
+      }
+    }, {
+        tableName: 'product',
+        timestamps: false
+    });
 
+
+  
+    Product.associate = (models) => {
+      // Relación muchos a muchos entre Product y Cart a través de Cart_Product
+      Product.belongsToMany(models.Cart, {
+        through: models.Cart_Product,
+        foreignKey: 'ID_Product',
+        onDelete: 'CASCADE'
+      });
+    };
+  
+    return Product;
+  };
+  
