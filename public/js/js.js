c5398aefe6ea38d02c059ab7cd831864f8014b97
hGauna <undrbug@gmail.com>
2024-09-27 20:59:43 -0300

    feat: Search input functionality added

--- public/js/js.js
+++ public/js/js.js
@@ -1,1 +1,22 @@
-// Archivos vacios para que se pueda subir la estructura de carpetas al github
+window.addEventListener("load", function () {
+	let input = document.querySelector("#input-search");
+	let actionSearch = document.querySelector("#search");
+
+	const handleSearch = () => {
+        let search = input.value;
+        if (!search) {
+            alert("El campo de búsqueda no puede estar vacío");
+		}else{
+                window.location.href = `/products/search/${search}`;
+        }
+	};
+    //disparador click
+	actionSearch.addEventListener("click", handleSearch);
+	//disparador enter
+    input.addEventListener("keypress", (e) =>{
+        e.preventDefault();
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    });
+});
