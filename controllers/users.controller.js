076b2d5ee1b8d3c68eef148f062039a6c41428fe
hGauna <undrbug@gmail.com>
2024-10-25 21:52:54 -0300

    Full user crud with persistence in db, fetch provinces, states for the form

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const router = require("express").Router();
-const services = require("../services/dataUsers.js");
 const bcrypt = require('bcryptjs');
-const dataUsers = require("../services/dataUsers.js");
+const dataCountries = require("../services/dataCountries.js");
 const {validationResult} = require('express-validator');
-const { use } = require("../routes/users.route.js");
+const db = require('../database/models/index.js');
+const servicesDB = require('../services/services_db.js');
+const { v4: uuidv4 } = require('uuid');
 
 const usersController = {
+    //renderizamos la vista de login
     login: (req, res) => {
         res.render('users/login.ejs', {title: 'Login'});
     },
-    loginCheck: (req, res) => {
+    //validamos los datos del login
+    loginCheck: async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.render("users/login.ejs", {
@@ -22,53 +22,75 @@ const usersController = {
             // res.send(errors.mapped());
         }
         
-        const user = dataUsers.findByEmail(req.body.email);
-        if (user) {
-            const errors =[];
-            const passwordMatch = bcrypt.compareSync(req.body.password, user.password);
-            if (passwordMatch) {
-                delete user.password;
-                req.session.userLogged = user;
-                if (req.body.rememberMe) {
-                    res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 60 });
+        try {
+            const user = await servicesDB.getByEmail(req.body.email);
+            if (user) {
+                const errors =[];
+                const passwordMatch = bcrypt.compareSync(req.body.password, user.HashPassword);
+                if (passwordMatch) {
+                    delete user.password;
+                    req.session.userLogged = user;
+                    if (req.body.rememberMe) {
+                        res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 60 });
+                    }
+                    res.redirect('/');
+    
+                } else {
+                    //Redireccione nuevamente al login en caso de error.
+                    return res.render("users/login.ejs", {
+                        title: "Login",
+                        errors: {
+                            password: {
+                                msg: "Las credenciales son inválidas",
+                            },
+                        },
+                        oldData: req.body,
+                    });
                 }
-                res.redirect('/');
-
-            } else {
+            }else {
                 //Redireccione nuevamente al login en caso de error.
                 return res.render("users/login.ejs", {
                     title: "Login",
                     errors: {
-                        password: {
-                            msg: "Las credenciales son inválidas",
+                        "email": {
+                                "msg": "El email no está registrado",
+                            }
                         },
-                    },
                     oldData: req.body,
                 });
             }
-        }else {
-            //Redireccione nuevamente al login en caso de error.
-            return res.render("users/login.ejs", {
-                title: "Login",
-                errors: {
-                    "email": {
-                            "msg": "El email no está registrado",
-                        }
-                    },
-                oldData: req.body,
-            });
+            
+        } catch (error) {
+            console.log("Error al buscar el usuario", error.message);
+        }
+
+    },
+    register: async (req, res) => {
+        try {
+            const statesResponse = dataCountries.getProvinciasJson();
+            const listaProvincias = await statesResponse;
+            const listaPaises = [{
+                nombre: "Argentina",
+            }];
+            res.render('users/register.ejs', {title: 'Register', listaPaises, listaProvincias});
+            // res.json({listaPaises});
+        } catch (error) {
+            console.log(`Error al traer lista de paises: ${error.message}`);
+            res.render('users/register.ejs', { title: 'Register', listaPaises: [], listaProvincias: [] });
         }
     },
-    register: (req, res) => {
-        res.render('users/register.ejs', {title: 'Register'});
-    },
-    newRegister: (req, res) => {
+    newRegister: async (req, res) => {
         const errors = validationResult(req);
+        const listaProvincias = await dataCountries.getProvinciasJson();
+        const listaPaises = [{ nombre: "Argentina" }];
+
         if (!errors.isEmpty()) {
             return res.render("users/register.ejs", {
                 title: "Register",
                 errors: errors.mapped(),
                 oldData: req.body,
+                listaPaises,
+                listaProvincias
             });
         }
         try {
@@ -77,9 +99,9 @@ const usersController = {
                 ? `/images/users/${req.file.filename}`
                 : "/images/users/default.png";
             // Accede a los campos del formulario desde req.body
-            const { firstName, lastName, email, password, category } = req.body;
-            
-            if (services.findByEmail(email)) {
+            const { firstName, lastName, email, phone, adress, country, state, password } = req.body;
+            console.log(imgUser);
+            if (await db.Customer.findOne({ where: { email: email } })) {
                 return res.render("users/register.ejs", {
                     title: "Register",
                     errors: {
@@ -88,32 +110,37 @@ const usersController = {
                         },
                     },
                     oldData: req.body,
+                    listaPaises,
+                    listaProvincias
                 });
             }
     
-            // Carga los usuarios existentes
-            let users = services.load();
             //Encriptamos la contraseña
-            let passwordEnc = bcrypt.hashSync(password, 10);
+            let HashPassword = bcrypt.hashSync(password, 10);
+            console.log(HashPassword);
             // Crea un nuevo usuario con todos los campos
             const newUser = {
-                id: users.length + 1,
-                firstName: firstName,
-                lastName: lastName,
+                first_name: firstName,
+                last_name: lastName,
                 email: email,
                 image: imgUser,
-                password: passwordEnc,
-                category: category
+                phone: phone,
+                adress: adress,
+                country: country,
+                state: state,
+                HashPassword: HashPassword,
+                isAdmin: 0,
+                isActive: 1,
+                verified: 1,
             };
     
-            // Agrega el nuevo usuario a la lista de usuarios y guarda los cambios
-            users.push(newUser);
-            services.save(users);
+            // Agrega el nuevo usuario a la DB de usuarios
+            db.Customer.create(newUser);
     
             // Redirige a la página principal después de guardar
             res.redirect("/users/login");
         } catch (error) {
-            console.log(error);
+            console.log(`Error al registrar el usuario: ${error.message}`);
             res.status(500).send("Error al registrar el usuario");
         }
     },
@@ -134,6 +161,3 @@ const usersController = {
 }
 
 module.exports = usersController;
-
-
-//controllers para users
