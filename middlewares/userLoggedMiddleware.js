1cbe9b8b3e1755409b8975668a3530f7fe8ca077
hGauna <undrbug@gmail.com>
2024-10-25 21:57:25 -0300

    try/catch implementation and user query to the db

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -1,18 +1,25 @@
-const dataUsers = require("../services/dataUsers.js");
+// const dataUsers = require("../services/dataUsers.js");
+const services_db = require("../services/services_db.js");
 
-function userLoggedMiddleware(req, res, next) {
+async function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
 
   //recuperamos el email del usuario desde la cookie, si es que hay una
   let userEmail = req.cookies.userEmail;
   
-  //buscamos al usuario por el email
-  let user = dataUsers.findByEmail(userEmail);
-
-  //Si encontramos al usuario, lo guardamos en la sesión
-  if (user) {
-    delete user.password;
-    req.session.userLogged = user;
+  try {
+    //buscamos al usuario por el email
+    let user = await services_db.getByEmail(userEmail);
+    // let user = dataUsers.findByEmail(userEmail);
+    console.log(user);
+    //Si encontramos al usuario, lo guardamos en la sesión
+    if (user) {
+      delete user.password;
+      req.session.userLogged = user;
+    }
+    
+  } catch (error) {
+    console.log("Error al buscar el usuario", error.message);
   }
 
   //Si hay un usuario logueado, lo pasamos a las vistas
