1e8bf7031071574f967d267e96e28f3bf16e2e02
hGauna <undrbug@gmail.com>
2024-10-29 23:47:38 -0300

    validations for the registration form

--- middlewares/validationsResgister.js
+++ middlewares/validationsResgister.js
@@ -6,6 +6,10 @@ const validationsRegister = [
     body('lastName').notEmpty().withMessage('El apellido es obligatorio'),
     body('email').isEmail().bail().withMessage('Ingresa un email valido').bail()
         .notEmpty().withMessage('El email es obligatorio'),
+    body('phone').notEmpty().withMessage('El teléfono es obligatorio'),
+    body('adress').notEmpty().withMessage('La dirección es obligatoria'),
+    body('country').notEmpty().withMessage('El país es obligatorio'),
+    body('state').notEmpty().withMessage('La ciudad es obligatoria'),
     body('password').notEmpty().withMessage('La contraseña es obligatoria').bail()
     .isLength({ min: 5 }).withMessage('La contraseña debe tener al menos 5 caracteres'),
     body('rePassword').notEmpty().withMessage('La contraseña es obligatoria').bail()
@@ -18,9 +22,8 @@ const validationsRegister = [
     }),
     body('image').custom((value, { req }) => {
         if (!req.file) {
-            throw new Error('La imagen es obligatoria');
+            return true;
         }
-        return true;
     })
 ];
 
