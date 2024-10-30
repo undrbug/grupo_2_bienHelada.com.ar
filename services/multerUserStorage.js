36bb025ad5a932b812159fd9c18fda3fd909f013
hGauna <undrbug@gmail.com>
2024-10-29 23:48:42 -0300

    filter to upload images by type

--- services/multerUserStorage.js
+++ services/multerUserStorage.js
@@ -1,16 +1,27 @@
-const multer = require('multer');
-const path = require('path');
-
+const multer = require("multer");
+const path = require("path");
 
 const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images/users');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${path.extname(file.originalname)}`);
-    }
+	destination: (req, file, cb) => {
+		cb(null, "./public/images/users");
+	},
+	filename: (req, file, cb) => {
+		cb(null, `${Date.now()}-${path.extname(file.originalname)}`);
+	},
 });
 
-const upload = multer({ storage: multerStorage });
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: (req, file, cb) => {
+		const acceptedExtensions = [".jpg", ".jpeg", ".png"];
+		const ext = path.extname(file.originalname);
+		if (!acceptedExtensions.includes(ext)) {
+			cb(new Error(`File extension ${ext} is not supported`));
+			// req.file = file;
+		} else {
+			cb(null, true);
+		}
+	}
+});
 
 module.exports = upload;
