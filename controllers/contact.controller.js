73c40db993c22adf4ac4b44e6f7b541cbfa23fe8
hGauna <undrbug@gmail.com>
2024-10-29 23:44:52 -0300

    feat: sendForgotPasswordEmail send email to recover password

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
-const { title } = require('process');
 
 
 const contactController = {
@@ -44,7 +43,53 @@ const contactController = {
         } catch (error) {
             console.log("ERROR: ", error.message);
         }
-
+    },
+    sendForgotPasswordEmail: async (req, res) => {
+        try {
+            const { email } = req.body;
+            const messageHtml = `<h2>Recuperación de contraseña</h2>
+            <h4>Has solicitado recuperar tu contraseña.</h4>
+            <p>Para recuperar tu contraseña, haz click en el siguiente enlace:</p>
+            <a href="${process.env.URL}/users/reset-password/${token}">Recuperar contraseña</a>`;
+            const transporter = nodemailer.createTransport({
+                host: process.env.HOST,
+                port: process.env.HOST_PORT,
+                secure: true,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            })
+            const mailOptions = {
+                from: process.env.FROM,
+                to: email,
+                subject: 'Recuperación de contraseña',
+                html: messageHtml,
+            }
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(`Error: ${error.message}`);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.redirect("/");
+                }
+            })
+        } catch (error) {
+            console.log("ERROR: ", error.message);
+        }
+    },
+    updatePassword: async (req, res) => {
+        try {
+            const { password } = req.body;
+            const hashPassword = bcrypt.hashSync(password, 10);
+            await servicesDB.updatePassword(hashPassword, req.params.id);
+            res.redirect('/');
+        } catch (error) {
+            console.log("ERROR: ", error.message);
+        }
     }
     
 }
