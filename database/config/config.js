362f126fa3db423953edbeb7dff985a4fb9b2d0e
Cainenk011 <yamal.e.maldonado@gmail.com>
2024-10-31 22:05:24 -0300

    footer changed and stylesheet modified

--- database/config/config.js
+++ database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
     username: "root",
-    password: "root",
+    password: "",
     database: "bienhelada",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
-    password: null,
+    password: "",
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: null,
+    password: "",
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql",
