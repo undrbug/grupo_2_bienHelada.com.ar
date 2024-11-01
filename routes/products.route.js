3747f9ab581ccd66f815c82c9ea2e49f15367857
hGauna <undrbug@gmail.com>
2024-10-31 22:43:32 -0300

    add authMiddleware to product routes

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const productsController = require('../controllers/products.controller.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 const upload = require('../services/multerProdcutsStorage.js');
 const { body } = require('express-validator');
 
@@ -24,23 +25,23 @@ router.get('/', productsController.getAllProducts);
 //muestra productos filtrados en el input_search
 router.get('/search/:search', productsController.searchProducts);
 //carrito de compras vista
-router.get('/cart/', productsController.productCartView);
+router.get('/cart',authMiddleware, productsController.productCartView);
 //carrito de compras metodo
-router.post('/cart/:id', productsController.productCartAdd);
+router.post('/cart/:id',authMiddleware, productsController.productCartAdd);
 //detalle de producto
 router.get('/productdetail/:id', productsController.productDetail);
 //Vista para agregar producto o dar de alta
-router.get('/add', productsController.productAddView);
+router.get('/add',authMiddleware, productsController.productAddView);
 //Metodo para agregar producto con su middleware para cargar imagen
-router.post('/add', upload.single('image'), validationsProducts, productsController.productAdd);
+router.post('/add',authMiddleware, upload.single('image'), validationsProducts, productsController.productAdd);
 //Vista para eliminar producto (no se si lo vamos a implementar)
 // router.get('/productdel', productsController.productDelView);
 //Metodo para eliminar producto
-router.delete('/productdel/:id', productsController.productDel);
+router.delete('/productdel/:id',authMiddleware, productsController.productDel);
 //Vista para modificar producto
-router.get('/productmod/:id', productsController.productModView);
+router.get('/productmod/:id',authMiddleware, productsController.productModView);
 //Metodo para modificar producto con su middleware para cargar imagen
-router.put('/productmod/:id', upload.single('image'), validationsProducts, productsController.productMod);
+router.put('/productmod/:id',authMiddleware, upload.single('image'), validationsProducts, productsController.productMod);
 
 router.get('/drinklist', productsController.drinkList);
 
