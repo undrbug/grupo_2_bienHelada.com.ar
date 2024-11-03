39be88d66c90efb36ee13095139df4a5a5bac751
hGauna <undrbug@gmail.com>
2024-11-03 14:58:36 -0300

    Redirect to login if the user wants to access with admin permissions

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -2,8 +2,8 @@ function adminMiddleware(req, res, next) {
     if (req.session.userLogged && req.session.userLogged.isAdmin) {
         return next(); 
     }
-    console.log(`El usuario ${req.session.userLogged.email} no tiene permisos de administrador`);
-    return res.redirect('/'); //
+    console.log(`El usuario no tiene permisos de administrador`);
+    return res.redirect('/users/login');
 }
 
 module.exports = adminMiddleware;
